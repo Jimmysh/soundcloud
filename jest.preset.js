35577f1f0732667b5575160d51377c0ef0e9816c
Jimmy Liu <hero63418@gmail.com>
2021-06-07 21:53:28 +0800

    123123

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,3 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset, coverageReporters: ['html', 'json'] };
+module.exports = { ...nxPreset, coverageReporters: ['html', 'json', 'lcov'] };
