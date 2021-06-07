381ac6b1952a14ff0423881993e7998e27633c22
Jimmy <hero63418@gmail.com>
2021-06-07 22:41:33 +0800

    Coverage (#2)

--- apps/soundcloud/src/app/app.component.spec.ts
+++ apps/soundcloud/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent1', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent]
