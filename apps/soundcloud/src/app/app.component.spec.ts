2416e54db26ed580cbc93027d4ef21389500398c
Jimmy Liu <hero63418@gmail.com>
2021-06-15 19:49:40 +0800

    123123

--- apps/soundcloud/src/app/app.component.spec.ts
+++ apps/soundcloud/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent1', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent]
