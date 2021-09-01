60898c47f59f492897cfc6af19db4df1458be033
Jimmy Liu <hero63418@gmail.com>
2021-09-02 00:03:43 +0800

    12312

--- tools/heroku/deploy.ts
+++ tools/heroku/deploy.ts
@@ -1,1 +1,7 @@
-console.log('deploy heroku !!!!');
+import * as yargs from 'yargs';
+
+const argv = yargs.option('appName', { type: 'string' }).argv;
+
+console.log('deploy heroku !', argv);
+
+// yarn run:tools tools/heroku/deploy
