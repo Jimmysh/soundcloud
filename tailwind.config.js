5b31dc0948ebedaa4625826556d2dea090783f92
Jimmy Liu <hero63418@gmail.com>
2021-09-08 10:58:21 +0800

    add taiwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  prefix: '',
+  mode: process.env.TAILWIND_MODE ? 'jit' : '',
+  purge: {
+    content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {
+    extend: {
+      display: ['group-hover']
+    }
+  },
+  plugins: []
+};
