5d5dfb221f179bcdf04015db648497da4e91f3b0
Jimmy Liu <hero63418@gmail.com>
2021-09-09 00:05:57 +0800

    123

--- apps/soundcloud/src/app/app.module.ts
+++ apps/soundcloud/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
