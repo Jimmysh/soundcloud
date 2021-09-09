ba002545be475d353278453d6e577d57c828197b
Jimmy Liu <hero63418@gmail.com>
2021-09-09 09:28:53 +0800

    21312

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   purge: {
     content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}']
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {},
   variants: {
     extend: {
