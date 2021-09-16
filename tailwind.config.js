3041f3bc6b9a06fd5c4812ec42288d7ad32c705f
Jimmy Liu <hero63418@gmail.com>
2021-09-16 12:00:14 +0800

    12312

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
   prefix: '',
   mode: process.env.TAILWIND_MODE ? 'jit' : '',
@@ -7,29 +8,28 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     colors: {
+      ...colors,
       background: {
-        color: '#fff',
-        step: {
-          50: 'var(--ion-color-step-50)',
-          100: 'var(--ion-color-step-100)',
-          150: 'var(--ion-color-step-150)',
-          200: 'var(--ion-color-step-200)',
-          250: 'var(--ion-color-step-250)',
-          300: 'var(--ion-color-step-300)',
-          350: 'var(--ion-color-step-350)',
-          400: 'var(--ion-color-step-400)',
-          450: 'var(--ion-color-step-450)',
-          500: 'var(--ion-color-step-500)',
-          550: 'var(--ion-color-step-550)',
-          600: 'var(--ion-color-step-600)',
-          650: 'var(--ion-color-step-650)',
-          700: 'var(--ion-color-step-700)',
-          750: 'var(--ion-color-step-750)',
-          800: 'var(--ion-color-step-800)',
-          850: 'var(--ion-color-step-850)',
-          900: 'var(--ion-color-step-900)',
-          950: 'var(--ion-color-step-950)'
-        }
+        color: '#000',
+        50: 'var(--ion-color-step-50)',
+        100: 'var(--ion-color-step-100)',
+        150: 'var(--ion-color-step-150)',
+        200: 'var(--ion-color-step-200)',
+        250: 'var(--ion-color-step-250)',
+        300: 'var(--ion-color-step-300)',
+        350: 'var(--ion-color-step-350)',
+        400: 'var(--ion-color-step-400)',
+        450: 'var(--ion-color-step-450)',
+        500: 'var(--ion-color-step-500)',
+        550: 'var(--ion-color-step-550)',
+        600: 'var(--ion-color-step-600)',
+        650: 'var(--ion-color-step-650)',
+        700: 'var(--ion-color-step-700)',
+        750: 'var(--ion-color-step-750)',
+        800: 'var(--ion-color-step-800)',
+        850: 'var(--ion-color-step-850)',
+        900: 'var(--ion-color-step-900)',
+        950: 'var(--ion-color-step-950)'
       },
       text: {
         color: '#000',
@@ -55,12 +55,7 @@ module.exports = {
           950: 'var(--ion-color-step-50)'
         }
       },
-      primary: {
-        default: 'var(--ion-color-primary)',
-        contrast: 'var(--ion-color-primary-contrast)',
-        shade: 'var(--ion-color-primary-shade)',
-        tint: 'var(--ion-color-primary-tint)'
-      },
+      primary: 'var(--ion-color-primary)',
       secondary: {
         default: 'var(--ion-color-secondary)',
         contrast: 'var(--ion-color-secondary-contrast)',
@@ -112,9 +107,7 @@ module.exports = {
     }
   },
   variants: {
-    extend: {
-      display: ['group-hover']
-    }
+    extend: {}
   },
   plugins: []
 };
